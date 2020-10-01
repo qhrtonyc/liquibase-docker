9336664af24204a8d4227bf22d47b37cc3ee2be7
Tony Cheng <tony.cheng@qhrte.com>
2020-10-01 14:57:24 -0600

    enforcing contexts input when performing update.

--- 3.6.3/docker-entrypoint.sh
+++ 3.6.3/docker-entrypoint.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 set -e
+
+# if contexts is not set, error out
+if [[ $@ == *'update' ]] && [[ $@ != *'--contexts'* ]]; then
+	echo "liquibase contexts must be set."
+	exit 1
+fi
 
 # first arg is `-*` (picks up `--*`, too)
 if [ "${1#-}" != "$1" ]; then
